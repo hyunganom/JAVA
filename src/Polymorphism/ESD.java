4920175df4e33122f7211a6b7fc81e1df9256f17
hyunganom <dlguswo0504@naver.com>
2022-06-14 20:18:52 +0900

    다형성 기본이론

--- src/Polymorphism/ESD.java
+++
@@ -1,7 +0,0 @@
-package Polymorphism;
-
-public class ESD {
-    public static void main(String[] args) {
-        System.out.println("하이");
-    }
-}
--- src/Polymorphism/Polymorphism1.java
+++ src/Polymorphism/Polymorphism1.java
@@ -1,7 +1,12 @@
 package Polymorphism;
 
 public class Polymorphism1 {
-    public static void main(String[] args) {
-        System.out.println("안녕");
-    }
+
 }
+//다형성
+//조상 타입 참조 변수로 자손 타입 객체를 다루는 것
+
+//객체와 참조변수의 타입이 일치할 때와 일치하지 않을 때의 차이?
+//SmartTv s = new SmartTv(); 랑 Tv t = new Tv(); 이거
+//참조변수와 인스턴스의 타입이 일치해야함 근데
+//Tv t = new SmartTv();<-- 조상 타입 참조변수로 자손 타입 인스턴스 참조
