f41871ef70350f99e21d73ad2dcec6ad3a93cb49
hyunganom <dlguswo0504@naver.com>
2022-06-14 15:45:05 +0900

    제어자

---
+++ src/OOP2/Modifier.java
@@ -0,0 +1,11 @@
+package OOP2;
+
+public class Modifier {
+}
+//제어자
+//클래스와 클래스의 멤버(멤버변수, 메서드)에 부가적인 의미 부여
+
+//접근제어자 : public, protected, private
+//그     외 : static, final, abstract, native, transient, synchronized, volatile, strictfp
+
+//하나의 대상에 여러 제어자를 같이 사용가능(접근 제어자는 하나만)
