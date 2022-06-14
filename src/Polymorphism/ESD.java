079692199c3b9acf1c76cf1331cc1669d7d56386
hyunganom <dlguswo0504@naver.com>
2022-06-14 18:27:42 +0900

    다형성

---
+++ src/Polymorphism/ESD.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class ESD {
+    public static void main(String[] args) {
+        System.out.println("하이");
+    }
+}
---
+++ src/Polymorphism/Polymorphism1.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        System.out.println("안녕");
+    }
+}
