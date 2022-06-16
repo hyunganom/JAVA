1a7c05b083921ca65d15ae5e6c2102a41270e640
hyunganom <dlguswo0504@naver.com>
2022-06-16 19:30:17 +0900

    Instanceof 연산자

---
+++ src/Polymorphism/Instance_of.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+public class Instance_of extends Car {
+
+}
+//instanceof 연산자
+//형변환 가능여부 확인에 사용, 가능하면 true 반환
+//형변환 전에 반드시 instanceof로 확인해야함
+//형변환하는 이유는 인스턴스의 원래기능을 모두 사용할려고, Car타입의 리모콘인 c로는 water()를 호출할수는 없으니깐 리모콘을 FireEngine타입으로 바꿔서 water()를 호출
+
+
