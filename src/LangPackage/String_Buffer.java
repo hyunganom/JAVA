0729a5036ed36d6cdfc94e5884c960a6df49a558
hyunganom <dlguswo0504@naver.com>
2022-06-28 22:02:12 +0900

    StringBuffer

---
+++ src/LangPackage/String_Buffer.java
@@ -0,0 +1,9 @@
+package LangPackage;
+
+public class String_Buffer {
+
+}
+//stringbuffer클래스
+//문자열을 저장하고 다루기 위한 클래스
+//-string처럼 문자형 배열(char[])을 내부적으로 가지고있다.
+//그러나 String과 달리 내용을 변경할 수 있다.(mutable)
---
+++ src/LangPackage/String_Builder.java
@@ -0,0 +1,8 @@
+package LangPackage;
+
+public class String_Builder {
+}
+//StringBuilder (동기화x)
+//StringBuffer는 동기화 되어있다. 멀티 쓰레드에 안전(thread-safe)
+//그런데 동기화가 멀티쓰레드가 아닌경우에는 불필요한 성능저하.
+
