218cd0b9b38b546500c675e8defb3790647df261
hyunganom <dlguswo0504@naver.com>
2022-07-11 20:10:38 +0900

    Comparator 와 Comparable

--- src/CollectionFramework/ComparatorAndComparable.java
+++ src/CollectionFramework/ComparatorAndComparable.java
@@ -18,11 +18,13 @@ public class ComparatorAndComparable {
 class Descending implements Comparator{
     @Override
     public int compare(Object o1, Object o2) {
-        if(o1 instanceof Comparable && o2 instanceof Comparable){}
-            Comparable c1 = (Comparable)o1;
-            Comparable c2 = (Comparable)o2;
+        if(o1 instanceof Comparable && o2 instanceof Comparable) {
+            Comparable c1 = (Comparable) o1;
+            Comparable c2 = (Comparable) o2;
             return c1.compareTo(c2) * -1;       //-1을 곱해서 기본 정렬방식의 역으로 변경한다.
                                                 //또는 c2.compareTo(c1)와 같이 순서를 바꿔도 된다.
+        }
+        return  -1;
     }
 }
 //Comparator 와 Comparable
