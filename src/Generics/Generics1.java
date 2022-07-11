77b7c14da510adae587e21a2e2f8013591f734eb
hyunganom <dlguswo0504@naver.com>
2022-07-12 06:26:16 +0900

    타입변수

--- src/Generics/Generics1.java
+++ src/Generics/Generics1.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 class Tv{}
 class Audio{}
 
-public class Type_Variable {
+public class Generics1 {
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
 //        ArrayList<Tv> list = new ArrayList<Tv>(); // Tv타입의 객체만 저장가능
