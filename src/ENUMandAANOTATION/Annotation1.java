a52302534df390ccc41e301ee66d388247f9d7d8
hyunganom <dlguswo0504@naver.com>
2022-07-14 03:54:53 +0900

    에너테이션

---
+++ src/ENUMandAANOTATION/Annotation1.java
@@ -0,0 +1,23 @@
+package ENUMandAANOTATION;
+
+
+
+
+public class Annotation1 {
+}
+//에너테이션
+//-주석처럼 프로그래밍 언어에 영향을 미치지 않으면, 유용한 정보를 제공
+
+//표준에너테이션
+//@Override 컴파일러에게 오버라이딩하는 메서드라는 것을 알린다.
+//@Deprecated   앞으로 사용하지 않을 것을 권장하는 대상에 붙인다.
+//@SuppressWarnings 컴파일러의 특정 경고메시기자 나타나지 않게 해준다.
+//@SafeVarargs  지네릭스 타입의 가변인자에 사용한다.
+//@FunctionalInterface  함수형 인터페이스라는 것을 알린다.
+//@Native   native메서드에서 참조되는 상수앞에 붙인다
+//
+//@Target    애너테이션이 적용가능한 대상을 지정하는데 사용한다.
+//@Documented   애너테이션 정보가 javadoc으로 작성된 문서에 포함되게 한다.
+//@Inherited    애너테이션이 자손 클래스에 상속되도록 한다.
+//@Relational   애너테이션이 유지되는 범위를 지정하는데 사용한다.
+//@Repeatable() 애너테이션을 반복해서 적용할 수 있게 한다.
